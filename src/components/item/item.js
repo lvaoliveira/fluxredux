04865e7991b682deeafc1f6694e5b92d97b12098
Lucas Oliveira <lucasoliveira@AMSloliveira2SG8WP.local>
2017-03-07 14:55:26 +0100

    cleaning structure

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -4,22 +4,57 @@ import Time from '../time/time';
 class Item extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      user: false,
-    };
+  }
+
+  renderContent() {
+    let content = '';
+    if (this.props.new) {
+      content = (
+        <form className="listItem--item listItem--form">
+          <input type="text" name="title" placeholder="Title" />
+          <textarea name="text" placeholder="text" />
+          <button type="submit" className="listItem--button">
+            <span className="listItem--button_label">
+              <i className="fa fa-floppy-o" aria-hidden="true" />
+            </span>Save
+          </button>
+        </form>
+      );
+    } else {
+      content = (
+        <div className="listItem--item">
+          <h2>{this.props.title}</h2>
+          <p>{this.props.text}</p>
+        </div>
+      );
+    }
+    return content;
   }
   render() {
-    const { time, date, title, text, origin } = this.props;
+    const { time, date, origin } = this.props;
     return (
       <div>
         <Time time={time} date={date} />
-        <i className={`fa fa-${origin}`} />
-        <div className="listItem--item">
-          <h2>{title}</h2>
-          <p>{text}</p>
-        </div>
+        <i className={`timeLine--icon fa fa-${origin}`} />
+        {this.renderContent()}
       </div>
     );
   }
 }
+Item.propTypes = {
+  new: React.PropTypes.bool,
+  title: React.PropTypes.string,
+  text: React.PropTypes.string,
+  time: React.PropTypes.string,
+  date: React.PropTypes.string,
+  origin: React.PropTypes.string,
+};
+Item.defaultProps = {
+  new: true,
+  title: '',
+  text: '',
+  time: '',
+  date: '',
+  origin: '',
+};
 export default Item;
--- src/components/listItem/listItem.js
+++ src/components/listItem/listItem.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import Item from '../item/item';
 
-import data from '../../data';
-
 class ListItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      user: false,
-    };
   }
+
   renderItems(item, i) {
-    console.log(item);
     return (
-      <Item time={item.time} date={item.date} title={item.title} text={item.text} origin={item.origin} key={i} />
+      <li key={i}>
+        <Item
+          time={item.time}
+          date={item.date}
+          title={item.title}
+          text={item.text}
+          origin={item.origin}
+          new={false}
+        />
+      </li>
     );
   }
+
   render() {
     return (
-      <ul className="listItem--wrapper">
-        <li>
-          { data.map(this.renderItems) }
-        </li>
-      </ul>
+      <div>
+        <ul className="listItem--wrapper">
+          <li>
+            <Item origin="file" />
+          </li>
+        </ul>
+      </div>
     );
   }
 }
